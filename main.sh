0e4a9ebf1fc8efecf5f14cf1cc3f906e31c94546
David Mack <david@sketchdeck.com>
2019-03-21 16:06:19 -0700

    adjust urls

--- main.sh
+++ main.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-PREFIX=gs://octavian-training2/compare_gan/imagenet32
+
 
 python3 compare_gan/main.py \
-	--model_dir $PREFIX/model/$RANDOM \
-	--tfds_data_dir $PREFIX/data \
+	--model_dir gs://octavian-training2/compare_gan/model/imagenet32/$RANDOM \
+	--tfds_data_dir gs://octavian-training2/compare_gan/data/imagenet/ \
 	$@
