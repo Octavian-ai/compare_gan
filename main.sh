487b7948bc39db4d855309e33072a49d9dd4a492
David Mack <david@sketchdeck.com>
2019-03-27 16:36:11 -0700

    add python path

--- main.sh
+++ main.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
+# Written to run on ctpu shells
 
-
-python3 compare_gan/main.py \
+PYTHONPATH=. python3 compare_gan/main.py \
 	--model_dir gs://octavian-training2/compare_gan/model/imagenet32/$RANDOM \
 	--tfds_data_dir gs://octavian-training2/compare_gan/data/imagenet/ \
 	$@
