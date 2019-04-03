fcaba96816325f89eb87ce7ef3df8530219834e6
David Mack <david@sketchdeck.com>
2019-04-03 08:19:31 -0700

    make ex

--- eval.sh
+++ eval.sh
@@ -2,4 +2,7 @@
 
 # I'm super lazy y'all
 
-./main.sh --nouse_tpu --gin_config example_configs/biggan_posenc_imagenet32.gin --model_dir gs://octavian-training2/compare_gan/model/imagenet32/posenc --schedule continuous_eval
+./main.sh --gin_config example_configs/biggan_posenc_imagenet32.gin \
+	 --model_dir gs://octavian-training2/compare_gan/model/imagenet32/posenc \
+	 --schedule continuous_eval \
+	 $@
