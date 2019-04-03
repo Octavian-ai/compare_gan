69130e6b15aa65b2ffcc19c430f028d96d518070
David Mack <david@sketchdeck.com>
2019-04-03 08:17:12 -0700

    Add eval

---
+++ eval.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# I'm super lazy y'all
+
+./main.sh --nouse_tpu --gin_config example_configs/biggan_posenc_imagenet32.gin --model_dir gs://octavian-training2/compare_gan/model/imagenet32/posenc --schedule continuous_eval
