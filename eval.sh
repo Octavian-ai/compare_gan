c9d992926b9128fe871dea2a93035afee9735191
David Mack <david@sketchdeck.com>
2019-04-03 17:34:36 -0700

    eval

--- eval.sh
+++ eval.sh
@@ -4,5 +4,5 @@
 
 ./main.sh --gin_config example_configs/biggan_posenc_imagenet32.gin \
 	 --model_dir gs://octavian-training2/compare_gan/model/imagenet32/posenc \
-	 --schedule continuous_eval \
+	 --schedule eval_last \
 	 $@
