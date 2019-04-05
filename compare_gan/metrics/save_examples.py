140db11ea9a60a7d829b5befef32300f5347f53b
David Mack <david@sketchdeck.com>
2019-04-05 13:38:15 -0700

    add grid

--- compare_gan/metrics/save_examples.py
+++ compare_gan/metrics/save_examples.py
@@ -45,13 +45,49 @@ class SaveExamplesTask():
 
   _LABEL = "save_examples"
 
+
+
+
+  def merge(self, images, size):
+    h, w = images.shape[1], images.shape[2]
+    if (images.shape[3] in (3,4)):
+        c = images.shape[3]
+        img = np.zeros((h * size[0], w * size[1], c))
+        for idx, image in enumerate(images):
+            i = idx % size[1]
+            j = idx // size[1]
+            img[j * h:j * h + h, i * w:i * w + w, :] = image
+        return img
+    elif images.shape[3]==1:
+        img = np.zeros((h * size[0], w * size[1]))
+        for idx, image in enumerate(images):
+            i = idx % size[1]
+            j = idx // size[1]
+            img[j * h:j * h + h, i * w:i * w + w] = image[:,:,0]
+        return img
+    else:
+        raise ValueError('in merge(images,size) images parameter ''must have dimensions: HxW or HxWx3 or HxWx4')
+
+
   def run_after_session(self, fake_dset, real_dest, step):
 
     tf.io.gfile.makedirs(os.path.join(FLAGS.example_dir, step))
     
-    for i in range(min(fake_dset.images.shape[0], FLAGS.example_count)):
+    n_images = fake_dset.images.shape[0]
+    
+    for i in range(min(n_images, FLAGS.example_count)):
       filename = os.path.join(FLAGS.example_dir, step, '%03d.png' % i)
       with tf.io.gfile.GFile(filename, 'w') as file:
         imageio.imwrite(file, fake_dset.images[i], format='png')
 
+    grid_size = (int(Math.sqrt(n_images))+1, int(Math.sqrt(n_images)))
+    grid = self.merge(fake_dset.images, grid_size)
+
+    filename = os.path.join(FLAGS.example_dir, step, 'grid.png')
+    with tf.io.gfile.GFile(filename, 'w') as file:
+      imageio.imwrite(file, grid, format='png')
+
     return {self._LABEL: FLAGS.example_count}
+
+
+    
