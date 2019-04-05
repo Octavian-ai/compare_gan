7c12a888da1246fedb4f3141daac604e1fe910b6
David Mack <david@sketchdeck.com>
2019-04-05 15:43:41 -0700

    eval

--- eval_imagenet128.sh
+++ eval_imagenet128.sh
@@ -1,7 +1,6 @@
 nohup ./main.sh \
 	--gin_config example_configs/biggan_imagenet128.gin \
     --model_dir gs://octavian-training2/compare_gan/model/imagenet128 \
-	--schedule eval_last \
 	--eval_batch_size 64 \
 	--force_label 429 \
 	--example_dir ./samples128 \
